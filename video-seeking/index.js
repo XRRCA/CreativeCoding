a18c7556d9d454eb01526fac2b9eedfb51d0cc27
Kevin Lee <kevin.m.lee@berkeley.edu>
2023-03-31 14:14:11 +0100

    make video-seeking canvas resize, work on mobile

--- video-seeking/index.js
+++ video-seeking/index.js
@@ -1,6 +1,10 @@
 const IMAGE_COUNT = 299;
 
 const canvas = document.getElementById('main-canvas');
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientHeight;
+const canvas_w = canvas.clientWidth;
+const canvas_h = canvas.clientHeight;
 const ctx = canvas.getContext('2d');
 const images = Array(IMAGE_COUNT).fill();
 images.forEach((_, i, arr) => {
@@ -9,10 +13,10 @@ images.forEach((_, i, arr) => {
 });
 
 images[0].addEventListener('load', (e) => {
-  ctx.drawImage(images[0], 0, 0, 400, 400);
+  ctx.drawImage(images[0], 0, 0, canvas_w, canvas_h);
 });
 
-canvas.addEventListener('mousemove', (event) => {
-  const idx = Math.floor((IMAGE_COUNT * event.offsetX) / 400);
-  ctx.drawImage(images[idx], 0, 0, 400, 400);
+canvas.addEventListener('pointermove', (event) => {
+  const idx = Math.floor((IMAGE_COUNT * event.offsetX) / canvas_w);
+  ctx.drawImage(images[idx], 0, 0, canvas_w, canvas_h);
 });
