9225ed5e4bc9b5cc7d18c6584229d873f6844821
Kevin Lee <kevin.m.lee@berkeley.edu>
2023-03-31 10:56:01 +0100

    load image 0 by default

--- video-seeking/index.js
+++ video-seeking/index.js
@@ -1,11 +1,15 @@
-const IMAGE_COUNT = 300;
+const IMAGE_COUNT = 299;
 
 const canvas = document.getElementById('main-canvas');
 const ctx = canvas.getContext('2d');
 const images = Array(IMAGE_COUNT).fill();
 images.forEach((_, i, arr) => {
   arr[i] = new Image();
-  arr[i].src = `imgs/${i}.jpg`;
+  arr[i].src = `imgs/${i + 1}.jpg`;
+});
+
+images[0].addEventListener('load', (e) => {
+  ctx.drawImage(images[0], 0, 0, 400, 400);
 });
 
 canvas.addEventListener('mousemove', (event) => {
